cc96fa61add70ea9a02180230bb55a8fab01965a
Rehuti <147031509+Rehuti@users.noreply.github.com>
2023-10-12 11:58:27 +0530

    Add files via upload

---
+++ github-file 1.py
@@ -0,0 +1,7 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=3
+>>> b=4
+>>> if(a>b):
+...     print("a is greater")
+... else:
